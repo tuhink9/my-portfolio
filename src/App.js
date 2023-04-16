39bb59f3db12d28949a5c5f018090c9359546608
putk9 <tuhinkabir523@gmail.com>
2023-04-16 20:52:06 +0600

    pages arranged

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import Footer from './Pages/Footer/Footer';
 import Navbar from './Pages/Header/Navbar';
 import Home from './Pages/Home/Home';
+
+import Services from './Pages/Services/Services';
 import About from './Pages/About/About';
+import Experiences from './Pages/Experiences/Experiences';
+import Works from './Pages/Works/Works';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
     <div className="App relative">
       <Navbar></Navbar>
       <Home></Home>
+      <About></About>
+            <Services></Services>
+            <Experiences></Experiences>
+            <Works></Works>
+            <Contact></Contact>
       <Footer></Footer>
-      <Routes>
-        <Route to="/about" element={<About></About>}></Route>
-      </Routes>
     </div>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import Services from '../Services/Services';
-import About from '../About/About';
-import Experiences from '../Experiences/Experiences';
-import Works from '../Works/Works';
-import Contact from '../Contact/Contact';
 
 const Home = () => {
     return (
         <div className='z-30'>
-            <About></About>
-            <Services></Services>
-            <Experiences></Experiences>
-            <Works></Works>
-            <Contact></Contact>
+            <h1>This is home</h1>
         </div>
     );
 };
