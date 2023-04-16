7e614560c3e67eff76cf4865838df53845768325
putk9 <tuhinkabir523@gmail.com>
2023-04-16 22:02:10 +0600

    pages arranged

--- src/Pages/Works/Works.js
+++ src/Pages/Works/Works.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Works = () => {
     return (
-        <div id='works' className="row-span-2  mx-32">
-          <div className="col-span-3">
+        <div id='works' className="grid grid-rows-2 gap-4 mb-8 mt-6 mx-32">
+          <div className="grid grid-cols-3">
             <div class="card w-96 bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
     <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
@@ -41,8 +41,20 @@ const Works = () => {
   </div>
 </div>
             </div>
-            <div className="col-span-3">
+            <div className="grid grid-cols-3">
             <div class="card w-96 bg-base-100 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div class="card w-96 bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
     <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
   </figure>
