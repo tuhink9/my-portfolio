3fa4e413b0e4e328a0221db6ba168006ce7e8a11
putk9 <tuhinkabir523@gmail.com>
2023-04-18 00:53:01 +0600

    img added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,9 +7,15 @@ const About = () => {
            <div class="card lg:card-side bg-base-100 shadow-xl">
             <div className="card-body">
               <h1 className='text-2xl font-bold'>About me</h1>
-            <figure><img src="#" alt="" /></figure>
+            <figure><a href="https://ibb.co/myyk9q0"><img src="https://i.ibb.co/vddp3Ph/Tuhin-2.jpg" alt="Tuhin-2" border="0"/></a></figure>
             </div>
   <div class="card-body">
+    <p>I'm Tuhin Kabir , also known as " Tuhin " , a self-learner of Programming, FullStack Web Development & WordPress Development from Narsingdi , Dhaka, Bangladesh. I love coding, solving problems, building amazing projects, reading books & many other cool stuffs.
+I believe, Knowledge has no limits!
+
+Age: 25 Years
+Country:  Bangladesh
+Freelance:  Available</p>
     <p>I'm Tuhin Kabir , also known as " Tuhin " , a self-learner of Programming, FullStack Web Development & WordPress Development from Narsingdi , Dhaka, Bangladesh. I love coding, solving problems, building amazing projects, reading books & many other cool stuffs.
 I believe, Knowledge has no limits!
 
@@ -21,26 +27,50 @@ Freelance:  Available</p>
     </div>
   </div>
   <div class="card-body">
-    <h2 class="card-title">New album is released!</h2>
+    <h2 class="card-title">Skills</h2>
+    <div className='para'>
+      <p className="skill">html css javascript:</p>
+      <p className="percent">60%</p>
+    </div>
     <div className='pline1'>
       <div className="line1"></div>
       <div className="aline"></div>
+    </div>
+    <div className='para'>
+      <p className="skill">html css javascript:</p>
+      <p className="percent">60%</p>
     </div>
     <div className='pline1'>
       <div className="line2"></div>
       <div className="bline"></div>
     </div>
+    <div className='para'>
+      <p className="skill">html css javascript:</p>
+      <p className="percent">60%</p>
+    </div>
     <div className='pline1'>
       <div className="line3"></div>
       <div className="cline"></div>
+    </div>
+    <div className='para'>
+      <p className="skill">html css javascript:</p>
+      <p className="percent">60%</p>
     </div>
     <div className='pline1'>
       <div className="line4"></div>
       <div className="dline"></div>
     </div>
+    <div className='para'>
+      <p className="skill">html css javascript:</p>
+      <p className="percent">60%</p>
+    </div>
     <div className='pline1'>
       <div className="line5"></div>
       <div className="eline"></div>
+    </div>
+    <div className='para'>
+      <p className="skill">html css javascript:</p>
+      <p className="percent">60%</p>
     </div>
     <div className='pline1'>
       <div className="line6"></div>
